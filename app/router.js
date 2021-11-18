e2bcc822c5f28a443fb7783082ed72ac598575a9
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-17 19:03:29 -0600

    get current listings

--- app/router.js
+++ app/router.js
@@ -1,9 +1,20 @@
 import Router from "express";
+import client from "./client.js";
+import config from "./config.js";
+
+const collection = client
+  .db(config.db.name)
+  .collection(config.db.collectionName);
 
 const router = new Router();
 
 router.get("/", (_, res) => {
   res.send("Hello API");
+});
+
+router.get("/current-listings", async (req, res) => {
+  const currentListings = await collection.find({}).limit(10).toArray();
+  res.json(currentListings);
 });
 
 export default router;
