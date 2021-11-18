c0233499ae568ab64834d21c4f062e3b1dd2ae71
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-17 18:53:22 -0600

    declare port and db info

---
+++ app/config.js
@@ -0,0 +1,14 @@
+// Provides config for the app in conjunction with dotenv
+// Config is the only place accesses 'dotenv'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT || 3000,
+  db: {
+    clientURL: process.env.DB_CLIENT_URL,
+    name: "sample_airbnb",
+    collectionName: "listingsAndReviews",
+  },
+};
